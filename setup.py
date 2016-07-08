6874fc5cd1b35300e9b415b87197e869fa3ddd52
Jesse Almanrode <jesse@almanrode.com>
2016-07-08 13:24:55 -0600

    Updated Requirements.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='sshreader',
       license='GNU Lesser General Public License v3 or later (LGPLv3+)',
       install_requires=['paramiko==2.0.1',
                         'future==0.15.2',
-                        'progressbar2==3.9.3',
+                        'progressbar2==3.9.4',
                         'python-hostlist==1.16',
                         'click==6.6'
                         ],
