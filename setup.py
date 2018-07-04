9973683bc97be61d37d0cc85b3451a07a2b26352
Jesse Almanrode <jesse@almanrode.com>
2018-07-04 14:10:31 -0600

    Bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup
 
 setup(name='sshreader',
-      version='4.4',
+      version='4.4.1',
       description='Multi-threading/processing wrapper for Paramiko',
       author='Jesse Almanrode',
       author_email='jesse@almanrode.com',
