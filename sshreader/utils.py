4533f4ddb3f0f4d98c111796801c7f29df78a852
Jesse Almanrode <jesse@almanrode.com>
2020-01-25 14:40:39 -0700

    Fixed bug when task_queue is not full when sub-process or thread is spawned.
    Had to add a delay loop which isn't pretty but it works.

--- sshreader/utils.py
+++ sshreader/utils.py
@@ -328,9 +328,13 @@ def sshread(serverjobs, pcount=None, tcount=None, progress_bar=False):
     task_queue = multiprocessing.Queue(maxsize=totaljobs)
     result_queue = multiprocessing.Queue(maxsize=totaljobs)
 
-    log.debug('filling input queue')
+    log.debug('filling task_queue')
     for job in serverjobs:
         task_queue.put(job)
+
+    while task_queue.empty():
+        log.debug('Waiting for task_queue to fill')
+        time.sleep(1)
 
     subs = list()  # Keep a list of processes/threads so we can join them later
 
