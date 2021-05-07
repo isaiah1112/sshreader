290baa2f96c0175f811bcd278ec84715781ddc37
Jesse Almanrode <jesse@almanrode.com>
2021-05-07 14:37:49 -0600

    Using twine to upload to PyPI.

--- deploy.bash
+++ deploy.bash
@@ -4,9 +4,10 @@ cat <<EOF >> ~/.pypirc
 [distutils]
 index-servers=pypi
 [pypi]
-repository=https://upload.pypi.org/pypi
+repository=https://upload.pypi.org/legacy/
 username=${PYPI_USERNAME}
 password=${PYPI_PASSWORD}
 EOF
-python setup.py sdist upload;
+python setup.py sdist bdist_wheel;
+twine upload dist/*;
 rm ~/.pypirc;
