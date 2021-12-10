4a4702cbdf390937679c4fa69dd87bcda38df4c6
Jesse Almanrode <jesse@almanrode.com>
2021-12-10 12:09:05 -0700

    Updated check for Python3.6 or later

--- sshreader/__init__.py
+++ sshreader/__init__.py
@@ -26,7 +26,7 @@ import sys
 
 if sys.version_info[0] < 3:
     raise Exception('Python2.x is no longer supported in this version of sshreader')
-elif sys.version_info[1] < 5:
+elif sys.version_info[1] < 6:
     raise Exception('Only Python 3.6 and later is supported in this version of sshreader')
 
 __author__ = 'Jesse Almanrode (jesse@almanrode.com)'
--- sshreader/ssh.py
+++ sshreader/ssh.py
@@ -30,7 +30,7 @@ __author__ = 'Jesse Almanrode (jesse@almanrode.com)'
 
 if sys.version_info[0] < 3:
     raise Exception('Python2.x is no longer supported in this version of sshreader')
-elif sys.version_info[1] < 5:
+elif sys.version_info[1] < 6:
     raise Exception('Only Python 3.6 and later is supported in this version of sshreader')
 
 # Globals
