4dfdcd38ac3d6093404d4b0f4e9d5566ac37200d
Jesse Almanrode <jesse@almanrode.com>
2022-01-02 16:21:51 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 
 setup(name='sshreader',
-      version='4.8.0',
+      version='4.8.2',
       description='Multi-threading/processing wrapper for Paramiko',
       author='Jesse Almanrode',
       author_email='jesse@almanrode.com',
