f7987fd0357a3354a766389e30e5ba7a333d2f79
Jesse Almanrode <jesse@almanrode.com>
2024-04-25 10:03:13 -0600

    Updated copyright dates

--- sshreader/__init__.py
+++ sshreader/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """A Python Package for parallelizing ssh connections via threading and multiprocessing.
 """
-# Copyright (C) 2015-2023 Jesse Almanrode
+# Copyright (C) 2015-2024 Jesse Almanrode
 #
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU Lesser General Public License as published by
