ada8e2dbf2a8662f07134ac6b1b6c162027a12ce
Jesse Almanrode <jesse@almanrode.com>
2025-07-02 12:47:14 -0600

    Fixed unused imports in module

--- sshreader/__init__.py
+++ sshreader/__init__.py
@@ -18,10 +18,6 @@
 # For backwards compatibility
 import logging
 
-from .customtypes import Command, EnvVars, Timeout, TimeoutTuple
-from .ssh import SSH, envvars
-from .utils import Hook, ServerJob, cpu_limit, echo, shell_command, sshread
-
 __author__ = 'Jesse Almanrode (jesse@almanrode.com)'
 __all__ = ['ssh', 'customtypes', 'utils']
 
